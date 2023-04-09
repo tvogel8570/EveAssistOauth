3972f6fa2953068c66f7698c6ddbd2201eef1dda
Timothy Vogel <Timothy@vogelware.com>
2023-04-09 10:26:32 -0500

    explore ServerAuthenticationSuccessHandler

--- src/main/java/com/eveassist/oauth/config/WebSecurityConfig.java
+++ src/main/java/com/eveassist/oauth/config/WebSecurityConfig.java
@@ -4,16 +4,28 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.ServerAuthenticationSuccessHandler;
 
 @Configuration
 public class WebSecurityConfig {
+	private final ServerAuthenticationSuccessHandler loginSuccessHandler;
+
+	public WebSecurityConfig(ServerAuthenticationSuccessHandler loginSuccessHandler) {
+		this.loginSuccessHandler = loginSuccessHandler;
+	}
+
 	@Bean
 	public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
 		http.authorizeExchange()
 				.anyExchange()
 				.authenticated()
 				.and()
-				.oauth2Login();
+				.oauth2Login()
+		//				.and()
+		//				.addFilterAfter(rememberMeAuthenticationFilter, SecurityWebFiltersOrder.AUTHENTICATION)
+		//				.authenticationSuccessHandler(loginSuccessHandler)
+		;
+
 		return http.build();
 	}
 
