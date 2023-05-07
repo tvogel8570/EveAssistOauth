e4ac7cfbccf51caa30e88606a4935311042a988a
Timothy Vogel <Timothy@vogelware.com>
2023-05-07 17:58:15 -0400

    add r2dbc persistence

--- src/main/java/com/eveassist/oauth/config/WebSecurityConfig.java
+++ src/main/java/com/eveassist/oauth/config/WebSecurityConfig.java
@@ -2,7 +2,11 @@ package com.eveassist.oauth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.r2dbc.core.DatabaseClient;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.client.R2dbcReactiveOAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authentication.ServerAuthenticationSuccessHandler;
 
@@ -20,13 +24,19 @@ public class WebSecurityConfig {
 				.anyExchange()
 				.authenticated()
 				.and()
+				//				.oauth2Login(Customizer.withDefaults())
 				.oauth2Login()
+				.authenticationSuccessHandler(loginSuccessHandler)
 		//				.and()
 		//				.addFilterAfter(rememberMeAuthenticationFilter, SecurityWebFiltersOrder.AUTHENTICATION)
-		//				.authenticationSuccessHandler(loginSuccessHandler)
 		;
 
 		return http.build();
 	}
 
+	//	@Bean
+	public ReactiveOAuth2AuthorizedClientService dbOauth2AuthorizedClientService(DatabaseClient databaseClient,
+			ReactiveClientRegistrationRepository clientRegistrationRepository) {
+		return new R2dbcReactiveOAuth2AuthorizedClientService(databaseClient, clientRegistrationRepository);
+	}
 }
