1eab6de829275a4095df9afeffe0fcc4d790cc72
Timothy Vogel <timothy@vogelware.com>
2023-05-08 07:31:25 -0400

    switched to postgres specific sql

--- src/main/resources/db/migration/V4__oauth_client_schema.sql
+++ src/main/resources/db/migration/V4__oauth_client_schema.sql
@@ -1,14 +1,14 @@
 CREATE TABLE oauth2_authorized_client
 (
-    client_registration_id  varchar(100)                                       NOT NULL,
-    principal_name          varchar(200)                                       NOT NULL,
-    access_token_type       varchar(100)                                       NOT NULL,
-    access_token_value      text                                               NOT NULL,
-    access_token_issued_at  timestamp with time zone                           NOT NULL,
-    access_token_expires_at timestamp with time zone                           NOT NULL,
-    access_token_scopes     varchar(1000)            DEFAULT NULL,
-    refresh_token_value     text                     DEFAULT NULL,
-    refresh_token_issued_at timestamp with time zone DEFAULT NULL,
-    created_at              timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    client_registration_id  varchar(100)                            NOT NULL,
+    principal_name          varchar(200)                            NOT NULL,
+    access_token_type       varchar(100)                            NOT NULL,
+    access_token_value      bytea                                   NOT NULL,
+    access_token_issued_at  timestamp                               NOT NULL,
+    access_token_expires_at timestamp                               NOT NULL,
+    access_token_scopes     varchar(1000) DEFAULT NULL,
+    refresh_token_value     bytea         DEFAULT NULL,
+    refresh_token_issued_at timestamp     DEFAULT NULL,
+    created_at              timestamp     DEFAULT CURRENT_TIMESTAMP NOT NULL,
     PRIMARY KEY (client_registration_id, principal_name)
 );
