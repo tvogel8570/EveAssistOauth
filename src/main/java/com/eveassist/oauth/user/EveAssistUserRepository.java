b2fbaed78554c6b164b1ed907985f75077ffe774
Timothy Vogel <timothy@vogelware.com>
2023-05-20 10:56:46 -0400

    beginning of changing to return Publisher<T> instead of types

--- src/main/java/com/eveassist/oauth/user/EveAssistUserRepository.java
+++ src/main/java/com/eveassist/oauth/user/EveAssistUserRepository.java
@@ -4,17 +4,17 @@ import com.eveassist.oauth.user.dto.EveAssistUserListDto;
 import com.eveassist.oauth.user.entity.EveAssistUser;
 import org.springframework.data.r2dbc.repository.Query;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-import java.util.List;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface EveAssistUserRepository extends ReactiveCrudRepository<EveAssistUser, Long> {
-	EveAssistUser findByEmailIgnoreCase(String email);
+	Mono<EveAssistUser> findByEmailIgnoreCase(String email);
 
-	EveAssistUser findByUniqueUser(String uniqueUser);
-
-	@Query("select e.unique_user, e.email, e.screen_name from eve_assist_user e")
-	List<EveAssistUserListDto> getAllUsersList();
+	Mono<EveAssistUser> findByUniqueUser(String uniqueUser);
 
 	@Query("select e.unique_user, e.email, e.screen_name from eve_assist_user e")
-	EveAssistUserListDto getUserList();
+	Flux<EveAssistUserListDto> getAllUsersList();
+
+	@Query("select e.unique_user, e.email, e.screen_name from eve_assist_user e")
+	Flux<EveAssistUserListDto> getUserList();
 }
--- src/test/java/com/eveassist/oauth/user/EveAssistUserRepositoryTest.java
+++ src/test/java/com/eveassist/oauth/user/EveAssistUserRepositoryTest.java
@@ -2,25 +2,27 @@ package com.eveassist.oauth.user;
 
 import com.eveassist.oauth.user.dto.EveAssistUserListDto;
 import com.eveassist.oauth.user.entity.EveAssistUser;
+import jakarta.validation.constraints.NotEmpty;
+import org.hibernate.validator.constraints.Length;
 import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-@DataJpaTest
+@DataR2dbcTest
 @Testcontainers
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @ActiveProfiles("test")
@@ -39,9 +41,26 @@ class EveAssistUserRepositoryTest {
 	}
 
 	@Autowired
-	TestEntityManager testEntityManager;
-	@Autowired
 	EveAssistUserRepository repository;
+	LocalDateTime testTime = LocalDateTime.now();
+
+	@BeforeEach
+	void setup() {
+		EveAssistUser eveAssistUser = EveAssistUser.builder()
+				.uniqueUser("123456789012345678901234567890")
+				.screenName("me")
+				.createTimestamp(testTime)
+				.updateTimestamp(testTime)
+				.email("tim@test.com").build();
+		repository.save(eveAssistUser);
+		eveAssistUser = EveAssistUser.builder()
+				.uniqueUser("123456789012345678901234567891")
+				.screenName("me1")
+				.createTimestamp(testTime)
+				.updateTimestamp(testTime)
+				.email("tim1@test.com").build();
+		repository.save(eveAssistUser);
+	}
 
 	@Test
 	void contextLoads() {
@@ -49,71 +68,47 @@ class EveAssistUserRepositoryTest {
 
 	@Test
 	void shouldRetrieveCorrectTime() {
-		LocalDateTime testTime = LocalDateTime.now();
-		EveAssistUser eveAssistUser = EveAssistUser.builder()
-				.uniqueUser("123456789012345678901234567890")
-				.screenName("me")
-				.createTimestamp(testTime)
-				.updateTimestamp(testTime)
-				.email("tim@test.com").build();
-		EveAssistUser retrievedUser = testEntityManager.persistFlushFind(eveAssistUser);
-		assertThat(retrievedUser.getCreateTimestamp()).isEqualToIgnoringNanos(eveAssistUser.getCreateTimestamp());
+		@Length(min = 30, max = 30) @NotEmpty String uniqueUser = "123456789012345678901234567890";
+		Mono<EveAssistUser> retrievedUser = repository.findByUniqueUser(uniqueUser);
+		StepVerifier.create(retrievedUser)
+				.expectNextMatches(t -> t.getUniqueUser().equals(uniqueUser))
+				.expectNextMatches(t -> t.getCreateTimestamp().isEqual(testTime));
 	}
 
 	@Test
 	void shouldReturnListOfAllUsers() {
-		LocalDateTime testTime = LocalDateTime.now();
-		EveAssistUser eveAssistUser = EveAssistUser.builder()
-				.uniqueUser("123456789012345678901234567890")
-				.screenName("me")
-				.createTimestamp(testTime)
-				.updateTimestamp(testTime)
-				.email("tim@test.com").build();
-		testEntityManager.persistAndFlush(eveAssistUser);
-		eveAssistUser = EveAssistUser.builder()
-				.uniqueUser("123456789012345678901234567891")
-				.screenName("me1")
-				.createTimestamp(testTime)
-				.updateTimestamp(testTime)
-				.email("tim1@test.com").build();
-		testEntityManager.persistAndFlush(eveAssistUser);
-		List<EveAssistUserListDto> allUserInfo = repository.getAllUsersList();
-		assertThat(allUserInfo).isNotNull();
-		assertThat(allUserInfo.size()).isEqualTo(2);
+		Flux<EveAssistUserListDto> allUserInfo = repository.getAllUsersList();
+		StepVerifier.create(allUserInfo)
+				.expectNextCount(2)
+				.expectComplete();
 	}
 
+/*
 	@Test
 	void shouldReturnUserDto() {
-		LocalDateTime testTime = LocalDateTime.now();
-		EveAssistUser eveAssistUser = EveAssistUser.builder()
-				.uniqueUser("123456789012345678901234567890")
-				.screenName("me")
-				.createTimestamp(testTime)
-				.updateTimestamp(testTime)
-				.email("tim@test.com").build();
+
 		EveAssistUser retrievedUser = testEntityManager.persistFlushFind(eveAssistUser);
 		EveAssistUserListDto userInfo = repository.getUserList();
 		assertThat(userInfo).isNotNull();
-		assertThat(retrievedUser.getEmail()).isEqualTo(userInfo.getEmail());
+		assertThat(userInfo.);
 	}
+*/
 
 	@Test
 	void shouldFindEmailCaseInsensitive() {
-		LocalDateTime testTime = LocalDateTime.now();
-		EveAssistUser eveAssistUser = EveAssistUser.builder()
-				.uniqueUser("123456789012345678901234567890")
-				.screenName("me")
-				.createTimestamp(testTime)
-				.updateTimestamp(testTime)
-				.email("tim@test.com").build();
-		testEntityManager.persistAndFlush(eveAssistUser);
-		EveAssistUser user = repository.findByEmailIgnoreCase("TIM@TEST.com");
-		assertThat(user).isNotNull();
+
+		Mono<EveAssistUser> user = repository.findByEmailIgnoreCase("TIM@TEST.com");
+		StepVerifier.create(user)
+				.expectNextCount(1)
+				.expectComplete();
+
 	}
 
+/*
 	@Test
 	void shouldThrowUserNotFoundException_whenSearchForEmailFails() {
 		EveAssistUser missingUser = repository.findByEmailIgnoreCase("asdf@asdf.com");
 		assertThat(missingUser).isNull();
 	}
+	*/
 }
